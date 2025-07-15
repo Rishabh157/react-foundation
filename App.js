20f11bdea06d1a70433f41fe1357d40ee181fd69
Rishabh157 <rishabhgour157@gmail.com>
2025-07-16 00:56:45 +0530

    made the three div structure using React.createElement

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>Hello I'm H1 tag</h1>
+    </div>
+</div>
+ */}
+
+
+const parent = React.createElement('div',
+    { id: 'parent' },
+    React.createElement('div',
+        { id: 'child' },
+        React.createElement('h1',
+            {}, "Hello I'm H1 tag"))
+)
+console.log('parent: ', parent);
+
 const header = React.createElement('h1', { id: 'name', className: '' }, 'Hello React Bro :)');
 console.log('header: ', header);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(header);
+root.render(parent);
