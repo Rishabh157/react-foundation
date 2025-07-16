e99035f6e45916cae1b7f6ae08ebb9192d5f4976
Rishabh157 <rishabhgour157@gmail.com>
2025-07-16 19:49:37 +0530

    appent one js problem

--- App.js
+++ App.js
@@ -1,35 +1,36 @@
 
-{/* <div id="parent">
-    <div id="child">
-        <h1>Hello I'm H1 tag</h1>
-        <h2>Hello I'm H1 tag</h2>
+{/* 
+    <div id="parent">
+        <div id="child">
+            <h1>Hello I'm H1 tag</h1>
+            <h2>Hello I'm H1 tag</h2>
+        </div>
+        <div id="child2">
+            <h1>Hello I'm H1 tag</h1>
+            <h2>Hello I'm H1 tag</h2>
+        </div>
     </div>
-    <div id="child2">
-        <h1>Hello I'm H1 tag</h1>
-        <h2>Hello I'm H1 tag</h2>
-    </div>
-</div>
- */}
+*/}
 
 
 const parent = React.createElement('div',
     { id: 'parent' },
- [
-       React.createElement('div',
-        { id: 'child' },
-        [
-            React.createElement('h1', {}, "Hello I'm H1 tag"),
-            React.createElement('h2', {}, "Hello I'm H1 tag")
-        ]
-    ),
-       React.createElement('div',
-        { id: 'child2' },
-        [
-            React.createElement('h1', {}, "Hello I'm H1 tag"),
-            React.createElement('h2', {}, "Hello I'm H1 tag")
-        ]
-    )
- ]
+    [
+        React.createElement('div',
+            { id: 'child' },
+            [
+                React.createElement('h1', {}, "Hello I'm H1 tag"),
+                React.createElement('h2', {}, "Hello I'm H1 tag")
+            ]
+        ),
+        React.createElement('div',
+            { id: 'child2' },
+            [
+                React.createElement('h1', {}, "Hello I'm H1 tag"),
+                React.createElement('h2', {}, "Hello I'm H1 tag")
+            ]
+        )
+    ]
 )
 console.log('parent: ', parent);
 
@@ -38,3 +39,15 @@ console.log('header: ', header);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
+
+const user = {
+    name: 'rishabh',
+    greet: function () {
+        // console.log(this.name);
+        setTimeout(function () {
+            console.log(this.name);
+        }, 1000)
+    }
+}
+
+user.greet();
