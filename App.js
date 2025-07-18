d03bac6b9a630e9f5e604882ace6f7fdfeee44f6
Rishabh157 <rishabhgour157@gmail.com>
2025-07-18 05:41:53 +0530

    added jsx

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
+
+
+const heading = React.createElement('h1', { id: 'name' }, "Hello World")
+
+// JSX Heading
+
+const jsxheading = <h1>Helllo World From JSX</h1>
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(jsxheading)
+
 {/* 
     <div id="parent">
         <div id="child">
@@ -14,41 +26,41 @@ import ReactDOM from 'react-dom/client';
     </div>
 */}
 
-const parent = React.createElement('div',
-    { id: 'parent' },
-    [
-        React.createElement('div',
-            { id: 'child' },
-            [
-                React.createElement('h1', {}, "Hello I'm H1 tag"),
-                React.createElement('h2', {}, "Hello I'm H1 tag")
-            ]
-        ),
-        React.createElement('div',
-            { id: 'child2' },
-            [
-                React.createElement('h1', {}, "Hello I'm H1 tag"),
-                React.createElement('h2', {}, "Hello I'm H1 tag")
-            ]
-        )
-    ]
-)
-console.log('parent: ', parent);
+// const parent = React.createElement('div',
+//     { id: 'parent' },
+//     [
+//         React.createElement('div',
+//             { id: 'child' },
+//             [
+//                 React.createElement('h1', {}, "Hello I'm H1 tag"),
+//                 React.createElement('h2', {}, "Hello I'm H1 tag")
+//             ]
+//         ),
+//         React.createElement('div',
+//             { id: 'child2' },
+//             [
+//                 React.createElement('h1', {}, "Hello I'm H1 tag"),
+//                 React.createElement('h2', {}, "Hello I'm H1 tag")
+//             ]
+//         )
+//     ]
+// )
+// console.log('parent: ', parent);
 
-const header = React.createElement('h1', { id: 'name', className: '' }, 'Hello React Bro :)');
-console.log('header: ', header);
+// const header = React.createElement('h1', { id: 'name', className: '' }, 'Hello React Bro :)');
+// console.log('header: ', header);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(parent);
 
-const user = {
-    name: 'rishabh',
-    greet: function () {
-        // console.log(this.name);
-        setTimeout(function () {
-            console.log(this.name);
-        }, 1000)
-    }
-}
+// const user = {
+//     name: 'rishabh',
+//     greet: function () {
+//         // console.log(this.name);
+//         setTimeout(function () {
+//             console.log(this.name);
+//         }, 1000)
+//     }
+// }
 
-user.greet();
+// user.greet();
